34fe48f4a404e0c99b4362526b4523c8f644bfdf
Imamdin Salimi <imamdinsalimi@gmail.com>
2022-02-18 13:16:02 +0530

    Modified first solution and readme.md

--- Say 'Hello World' in Python/solution.py
+++ Say 'Hello World' in Python/solution.py
@@ -1,1 +1,7 @@
-print("Hello, World")
+# In here we will be doing only two method however, there are various other methods through which you can print in python
+# First Method
+print("Hello, World!")
+
+#Second Solution
+my_string = "Hello, World!"
+print(my_string)
